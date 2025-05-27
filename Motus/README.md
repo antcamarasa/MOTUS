0b63f6d613722d16fad3d4e91ce82826fb0def8e
Antoine Camarasa <camarasa.ltd@gmail.com>
2025-05-27 10:40:51 +0200

    Add files via upload

---
+++ Motus/README.md
@@ -0,0 +1,15 @@
+# Motus
+Auteur: antcamarasa
+
+## Backend
+```
+cd backend
+mvn spring-boot:run
+```
+
+## Frontend
+```
+cd frontend
+npm install
+npm start
+```
---
+++ Motus/backend/src/main/java/com/motus/MotusApplication.java
@@ -0,0 +1,9 @@
+package com.motus;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class MotusApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MotusApplication.class, args);
+    }
+}
