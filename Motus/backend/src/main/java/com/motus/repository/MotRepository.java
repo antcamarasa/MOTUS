135dcc230b447b6378e29d2dc25a4017ba07ed59
Antoine Camarasa <camarasa.ltd@gmail.com>
2025-05-27 12:03:07 +0200

    Delete Motus directory

--- Motus/backend/src/main/java/com/motus/repository/MotRepository.java
+++
@@ -1,7 +0,0 @@
-package com.motus.repository;
-import com.motus.entity.Mot;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-public interface MotRepository extends JpaRepository<Mot,Long> {
-    List<Mot> findByLongueurAndDifficulte(int longueur, String difficulte);
-}
--- Motus/backend/src/main/java/com/motus/repository/ScoreRepository.java
+++
@@ -1,7 +0,0 @@
-package com.motus.repository;
-import com.motus.entity.Score;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-public interface ScoreRepository extends JpaRepository<Score,Long> {
-    List<Score> findTop10ByOrderByScoreDesc();
-}
